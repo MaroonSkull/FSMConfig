c615c9f6c8e0a5efffd22edb76ef2dff3517fd69
MaroonSkull <33577562+MaroonSkull@users.noreply.github.com>
2026-01-19 23:54:36 +0300

    tests: add test_config_parser stub

---
+++ tests/test_config_parser.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include "fsmconfig/config_parser.hpp"
+#include "fsmconfig/types.hpp"
+
+namespace fsmconfig {
+
+/**
+ * @file test_config_parser.cpp
+ * @brief Тесты для ConfigParser
+ */
+
+class ConfigParserTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Реализация будет добавлена в следующих подзадачах
+    }
+
+    void TearDown() override {
+        // Реализация будет добавлена в следующих подзадачах
+    }
+
+    ConfigParser parser;
+};
+
+TEST_F(ConfigParserTest, ParseEmptyConfig) {
+    // Тест будет реализован в следующих подзадачах
+    EXPECT_TRUE(true);
+}
+
+TEST_F(ConfigParserTest, ParseSimpleConfig) {
+    // Тест будет реализован в следующих подзадачах
+    EXPECT_TRUE(true);
+}
+
+TEST_F(ConfigParserTest, ParseInvalidConfig) {
+    // Тест будет реализован в следующих подзадачах
+    EXPECT_TRUE(true);
+}
+
+TEST_F(ConfigParserTest, ValidateValidConfig) {
+    // Тест будет реализован в следующих подзадачах
+    EXPECT_TRUE(true);
+}
+
+} // namespace fsmconfig
